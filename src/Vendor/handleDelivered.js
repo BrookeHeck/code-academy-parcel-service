8979f1c7c894735e03bcc382b20c1fa7bf1fc900
Brooke Heck <b19heck@gmail.com>
2022-10-05 17:35:17 +0000

    clients join rooms

--- src/Vendor/handleDelivered.js
+++ src/Vendor/handleDelivered.js
@@ -3,7 +3,7 @@
 module.exports = (socket) => (payload) => {
   console.log(payload.message);
   payload.event = 'received';
-  payload.message = `${payload.store} received order id: ${payload.orderId}`;
+  payload.message = `Recipient received order id: ${payload.orderId}`;
   socket.emit('log', payload);
   socket.emit('complete', payload);
 };
--- src/server.js
+++ src/server.js
@@ -8,20 +8,19 @@ const logger = require('./logEvents');
 
 const caps = server.of('/caps');
 
-caps.on('connection', (socket) => {
+caps.on('connection', async (socket) => {
   
   socket.on('log', logger);
 
-  // socket.on('pickup', (payload) => {
-  //   payload.room = `${payload.store}:${payload.orderId}`;
-  //   socket.join(`${payload.room}`);
-  //   console.log(`Socket ${payload.room}`);
-  // });
+  socket.on('pickup', payload => {
+    socket.join(`${payload.store}`);
+    console.log(`${socket.id}`);
+  });
 
-  // socket.on('deliver', (payload) => {
-  //   socket.join(`${payload.room}`);
-  //   console.log(`Socket ${socket.id} joined room ${payload.room}`);
-  // });
+  socket.on('in-transit', payload => {
+    socket.join(`${payload.store}`);
+    console.log(`${socket.id}`);
+  });
 
   observables.forEach(event => {
     socket.on(event, (payload) => socket.broadcast.emit(event, payload));
